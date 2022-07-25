c360742f49fd810882cea7e8d133d2d3804470c5
Harutyun Piloyan <102599567+Piloyan@users.noreply.github.com>
2022-07-25 14:42:12 +0400

    Update TransactionController.cs

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -37,7 +37,6 @@ namespace BankTransactions.Controllers
 
         // POST: Transaction/AddOrEdit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddOrEdit([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] Transaction transaction)
